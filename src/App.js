63c7340e0144cd9e64030896c84c690de3caff5c
yusufkarakaya <yusufkarakaya92@gmail.com>
2022-08-13 09:21:52 -0700

    project completed

--- src/App.js
+++ src/App.js
@@ -7,23 +7,63 @@ class App extends Component {
 
     this.state = {
       people: [],
+      previousPage: "",
+      nextPage: "",
     };
   }
 
+  performApiRequest(URL) {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState((prevState) => ({
+          ...prevState,
+          people: data.results,
+          previousPage: data.previous ? data.previous : "",
+          nextPage: data.next ? data.next : "",
+        }));
+      });
+  }
+
   componentDidMount() {
-    fetch(`https://swapi.py4e.com/api/people/`)
-      .then((response) => response.json())
-      .then((people) => this.setState({ people: people.results }));
+    this.performApiRequest("https://swapi.py4e.com/api/people/");
+  }
+
+  pageChange(URL) {
+    if (URL) {
+      this.performApiRequest(URL);
+    }
   }
 
   render() {
-    const { people } = this.state;
-
+    const { people, previousPage, nextPage } = this.state;
     return (
       <div>
         <h1 className="tc pa2 f1">The Star Wars API</h1>
         <hr></hr>
         <CardList people={people} />
+        <div className="tc">
+          {previousPage ? (
+            <button
+              className="white b pv2 ph3 bg-gray hover-bg-silver bt-0 br-0 bl-0 bb bw2 b--mid-gray br2 ma2"
+              onClick={() => {
+                this.pageChange(previousPage);
+              }}
+            >
+              Previous Page
+            </button>
+          ) : null}
+          {nextPage ? (
+            <button
+              className="white b pv2 ph3 bg-gray hover-bg-silver bt-0 br-0 bl-0 bb bw2 b--mid-gray br2 ma2"
+              onClick={() => {
+                this.pageChange(nextPage);
+              }}
+            >
+              Next Page
+            </button>
+          ) : null}
+        </div>
       </div>
     );
   }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,22 +3,24 @@ import Card from "./Card";
 
 const CardList = ({ people }) => {
   return (
-    <div className="flex flex-wrap justify-center">
-      {people.map((person) => {
-        return (
-          <Card
-            name={person.name}
-            height={person.height}
-            mass={person.mass}
-            hair_color={person.hair_color}
-            skin_color={person.skin_color}
-            eye_color={person.eye_color}
-            birth_year={person.birth_year}
-            gender={person.gender}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="flex flex-wrap justify-center">
+        {people.map((person, index) => {
+          return (
+            <Card
+              name={person.name}
+              height={person.height}
+              mass={person.mass}
+              hair_color={person.hair_color}
+              skin_color={person.skin_color}
+              eye_color={person.eye_color}
+              birth_year={person.birth_year}
+              gender={person.gender}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
